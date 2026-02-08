a2ee85a0fb34214d3a3095f8aed3ac3e17d185cb
vandy135 <titan@fob-aspen>
2026-02-08 18:55:27 -0500

    git: add user config (vandy135, titan@fob-aspen) to both hosts

--- hosts/fob-aspen/home.nix
+++ hosts/fob-aspen/home.nix
@@ -4,5 +4,15 @@
 
   programs.home-manager.enable = true;
 
+  programs.git = {
+    enable = true;
+    userName = "vandy135";
+    userEmail = "titan@fob-aspen";
+    extraConfig = {
+      init.defaultBranch = "main";
+      push.autoSetupRemote = true;
+    };
+  };
+
   home.stateVersion = "25.11";
 }
