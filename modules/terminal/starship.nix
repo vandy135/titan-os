080db10cae84d69fdd694eeeb9ce1a38329654fc
vandy135 <titan@fob-aspen>
2026-02-08 16:25:25 -0500

    starship: add space between git status icons and counts

--- modules/terminal/starship.nix
+++ modules/terminal/starship.nix
@@ -74,16 +74,16 @@ in
 
               git_status = {
                 format = "$all_status$ahead_behind";
-                conflicted = "[$count](bold red) ";
-                ahead = "[$count](bold green) ";
-                behind = "[$count](bold red) ";
-                diverged = "[$ahead_count](bold green)[$behind_count](bold red) ";
-                untracked = "[$count](bold blue) ";
-                stashed = "[$count](bold cyan) ";
-                modified = "[$count](bold yellow) ";
-                staged = "[$count](bold green) ";
-                renamed = "[$count](bold purple) ";
-                deleted = "[$count](bold red) ";
+                conflicted = "[ $count](bold red) ";
+                ahead = "[ $count](bold green) ";
+                behind = "[ $count](bold red) ";
+                diverged = "[ $ahead_count](bold green)[ $behind_count](bold red) ";
+                untracked = "[ $count](bold blue) ";
+                stashed = "[ $count](bold cyan) ";
+                modified = "[ $count](bold yellow) ";
+                staged = "[ $count](bold green) ";
+                renamed = "[ $count](bold purple) ";
+                deleted = "[ $count](bold red) ";
               };
 
               # Language modules (right prompt)
