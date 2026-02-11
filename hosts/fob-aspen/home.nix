1b8895a2b6b2af2c935d2dd5d0783dd6195b2323
vandy135 <titan@fob-aspen>
2026-02-10 20:46:52 -0500

    ssh: enableDefaultConfig=false to silence deprecation warning

--- hosts/fob-aspen/home.nix
+++ hosts/fob-aspen/home.nix
@@ -18,6 +18,7 @@
 
   programs.ssh = {
     enable = true;
+    enableDefaultConfig = false;
     matchBlocks."*" = {
       extraOptions.AddKeysToAgent = "yes";
       identityFile = [ "~/.ssh/github" ];
